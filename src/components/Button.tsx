4f99cf885ff5571c5d9480e9fabe6375424a7624
Joilson M S Lopes <joilsonmirandasl@hotmail.com>
2021-06-23 01:18:06 -0300

    chore: add aula 02

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,16 +1,13 @@
-import { useState } from "react"
+import { ButtonHTMLAttributes } from 'react';
 
-export function Button() {
-  const [counter, setCounter] = useState(0);
-  console.log(useState())
+import '../styles/button.scss';
 
-  function increment() {
-    setCounter( counter + 1)
-  }
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 
+export function Button(props: ButtonProps) {
   return (
     <div>
-      <button onClick={increment}>{counter}</button>
+      <button className="button" {...props} />
     </div>
   )
 }
